5dc3ceef77c03c5df878ac56af88f4cae2ba3c3a
Paul Kramme <pjkramme@gmail.com>
2016-08-14 17:22:16 +0200

    Add command function

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import sys
 
-print("MC Package Manager\n")
-if len(sys.argv) == 1:
-	print("Insufficient args\n")
-	exit()
-elif len(sys.argv) == 2:
-	pass
-else:
-	print("Too much args\n")
+print("MC Package Manager")
+if len(sys.argv) > 1 and len(sys.argv) < 5:
+	if sys.argv[1] == "install":
+		#iterating through mod list
+		print("Installing...")
+	elif sys.argv[1] == "update":
+		#look for changes in the package list
+		print("Updating...")
+	elif sys.argv[1] == "upgrade":
+		#upgrade installed mods
+		print("Upgrading...")
+	else:
+		print("No command found.")
