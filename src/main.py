66ab9923a60a079ba1488655c044af7f9f64357d
Paul Kramme <pjkramme@gmail.com>
2016-08-15 22:42:08 +0200

    Change to csv

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 import sys
-import json
+import csv
 import os.path
 
 print("MC Package Manager")
 if len(sys.argv) > 1 and len(sys.argv) < 5:
+
+
+
 	if sys.argv[1] == "install":
 		#iterating through mod list
 		print("Installing...")
@@ -13,6 +16,18 @@ if len(sys.argv) > 1 and len(sys.argv) < 5:
 		else:
 			print("Package list not found. Do something")
 		#load csv
+		with open('pkglist.csv') as packagelist:
+			data = csv.DictReader(packagelist, delimiter=',')
+			for row in data:
+				#print(row['pkg'], row['version'], row['link'])
+				if sys.argv[2] in row['pkg']:
+					print("Package found!")
+				else:
+					#useless.
+					print("Not found.")
+					#pass
+
+
 	elif sys.argv[1] == "update":
 		#look for changes in the package list
 		print("Updating...")
@@ -22,6 +37,8 @@ if len(sys.argv) > 1 and len(sys.argv) < 5:
 			#download package list...
 			print("Package list not found. Downloading...")
 			#blabla
+
+
 				
 	elif sys.argv[1] == "upgrade":
 		#upgrade installed mods
