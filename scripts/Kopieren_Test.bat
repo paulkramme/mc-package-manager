8635535c868d354f060d5b47004b1dd649dff6c6
Paul Kramme <pjkramme@gmail.com>
2016-08-16 22:04:32 +0200

    Add 3rdparty bash scripts

---
+++ scripts/Kopieren_Test.bat
@@ -0,0 +1,8 @@
+@echo off
+
+COPY C:\Users\DeadSlayer\Desktop\Test_Ordner_1 C:\Users\DeadSlayer\Desktop\Test_Zielverzeichnes_1
+COPY C:\Users\DeadSlayer\Desktop\Test_Ordner_2 C:\Users\DeadSlayer\Desktop\Test_Zielverzeichnes_1
+
+pause
+
+exit
---
+++ scripts/Loeschen_Test.bat
@@ -0,0 +1,10 @@
+@echo
+
+@echo off
+
+DEL C:\Users\DeadSlayer\Desktop\Test_Zielverzeichnes_1\Penetrations_test_1.txt
+DEL C:\Users\DeadSlayer\Desktop\Test_Zielverzeichnes_1\Penetrations_test_2.txt
+
+pause
+
+exit
